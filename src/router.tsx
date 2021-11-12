3109ad0edd849de20e283cc4850226700cc81012
Henrique Nery <88451907+henriquenery1@users.noreply.github.com>
2021-11-12 17:07:58 -0300

    Update router.tsx

--- src/router.tsx
+++ src/router.tsx
@@ -7,9 +7,11 @@ import { FormStep3 } from './pages/FormStep3';
 export const Router = () => {
     return (
         <BrowserRouter>
-            <Route path="/" exact component={FormStep1} />
-            <Route path="/step2" component={FormStep2} />
-            <Route path="/step3" component={FormStep3} />
+            <Routes>
+                <Route path="/" element={<FormStep1 />} />
+                <Route path="/step2" element={<FormStep2 />} />
+                <Route path="/step3" element={<FormStep3 />} />
+            </Routes>
         </BrowserRouter>
     );
 }
